ce6a3529ec678902dc55241b8f917aadfcfda2c9
bocchi277 <astra277353@gmail.com>
2026-02-23 20:42:33 +0530

    Modify the code

--- Project2.py
+++ Project2.py
@@ -1,29 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
-import sys
-
-
-def loadUrl(userUrl):
-    try:
-        headers = {
-            "User-Agent": "Mozilla/5.0"
-        }
-        htmlFile  = requests.get(userUrl,headers=headers, timeout=4)
-    except requests.RequestException:
-        return None
-    if(htmlFile.status_code == 200):
-        content = htmlFile.text
-        soup = BeautifulSoup(content, 'html.parser')
-        title_tag  = soup.find('title')
-        title = title_tag.text if title_tag else "No Tag Found"
-        bodyTag = soup.find('body')
-        bodyText = bodyTag.get_text(separator=" ", strip=True) if bodyTag else "Body is Not there"
-        links = [link.get('href') for link in soup.find_all('a') if link.get('href')]
-        return {"title": title, "bodyText" : bodyText, "Links": links}
-    else:
-        print(f"Error the status code is {htmlFile.status_code}, either enter valid url or try again")
-        return None
-
+from Project1 import loadUrl, sys
 def countFreq(bodyText):
     bodyText = bodyText.lower()
     cleaned = []
@@ -101,12 +76,7 @@ def simhashCompare(url1, url2):
 if __name__ == "__main__":
     try:
         userUrl = sys.argv
-        if len(userUrl) == 2:
-            userUrl = sys.argv[1]
-            content = loadUrl(userUrl)
-            for i in content:
-                print(f"The {i} is --> {content[i]}")
-        elif len(userUrl) >= 3:
+        if len(userUrl) >= 3:
             userUrl1 = sys.argv[1]
             userUrl2 = sys.argv[2]
             simhashCompare(userUrl1, userUrl2)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Read Please
+So Project1 have code for loading a url from command line and output the anchor tag links, body text, title of the required page
+Project2 consist of multiple function to compute the simhash and compare the simhash of two requested urls from the command line..
+---Other than that---
 Nothing Much, Just Make sure to run requirements.txt
